2b6055d2408a4d9fcdfe8aceca777f0197f8ee91
nino <Ninokeldishvili@gmail.com>
2024-12-31 15:46:45 +0400

    Add test getOrInitTrackingPeriod loads existing TrackingPeriod

--- tests/cys-flr.test.ts
+++ tests/cys-flr.test.ts
@@ -12,6 +12,9 @@ import { handleTransfer } from "../src/cys-flr";
 import { createTransferEvent } from "./cys-flr-utils";
 import { getOrInitTrackingPeriod } from "../src/cys-flr";
 let entityId: Bytes ;
+import {
+  TrackingPeriod,
+} from "../generated/schema";
 
 describe('Functions unit tests', () => {
   test("getOrInitTrackingPeriod initializes new TrackingPeriod", () => {
@@ -21,6 +24,19 @@ describe('Functions unit tests', () => {
 
     assert.fieldEquals("TrackingPeriod", Bytes.fromUTF8(period).toHexString(), "period", period);
     assert.fieldEquals("TrackingPeriod", Bytes.fromUTF8(period).toHexString(), "totalApprovedTransfersIn", "0");
+  });
+
+  test("getOrInitTrackingPeriod loads existing TrackingPeriod", () => {
+    let period = "JAN_2";
+    let id = Bytes.fromUTF8(period).toHexString();
+
+    let trackingPeriod = new TrackingPeriod(Bytes.fromUTF8(period));
+    trackingPeriod.period = period;
+    trackingPeriod.totalApprovedTransfersIn = BigInt.fromI32(100);
+    trackingPeriod.save();
+
+    getOrInitTrackingPeriod(period);
+    assert.fieldEquals("TrackingPeriod", id, "totalApprovedTransfersIn", "100");
   });
 });
 
