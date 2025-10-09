51953df27d4a0e2733f8648ef9dc344f840c321f
rouzwelt <rouzweltt@gmail.com>
2025-10-09 23:55:55 +0000

    update
    impl receipt balances

--- tests/receipt.test.ts
+++ tests/receipt.test.ts
@@ -1,18 +1,14 @@
-import { Address, BigInt, Bytes, store } from "@graphprotocol/graph-ts";
-import { handleReceiptTransferBatch, handleReceiptTransferSingle } from "../src/receipt";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { createReceiptTransferBatchEvent, createReceiptTransferSingleEvent } from "./utils";
 import { test, assert, describe, clearStore, beforeAll, afterAll } from "matchstick-as/assembly/index";
+import { createReceiptOwnerBalanceId, handleReceiptTransferBatch, handleReceiptTransferSingle } from "../src/receipt";
 
 const FROM = Address.fromString("0x0000000000000000000000000000000000000001");
 const TO = Address.fromString("0x0000000000000000000000000000000000000002");
 const OPERATOR = Address.fromString("0x0000000000000000000000000000000000000003");
 const RECEIPT_ADDRESS = Address.fromString("0xd387FC43E19a63036d8FCeD559E81f5dDeF7ef09");
 
-function getId(hash: Bytes, logIndex: BigInt, index: i32): Bytes {
-    return hash.concatI32(logIndex.toI32()).concatI32(index);
-}
-
-describe("ReceiptTransfer handling", () => {
+describe("ReceiptOwnerBalance handling", () => {
   beforeAll(() => {
     clearStore();
   });
@@ -22,7 +18,7 @@ describe("ReceiptTransfer handling", () => {
   });
 
   describe("single", () => {
-    test("should build and store ReceiptTransfer item from SingleTransfer event", () => {
+    test("should build, apply balance changes and store ReceiptOwnerBalance items from SingleTransfer event", () => {
       let transferEvent = createReceiptTransferSingleEvent(
         RECEIPT_ADDRESS,
         FROM,
@@ -33,20 +29,26 @@ describe("ReceiptTransfer handling", () => {
       );
       handleReceiptTransferSingle(transferEvent);
 
-      const id = getId(transferEvent.transaction.hash, transferEvent.logIndex, 0).toHexString();
+      assert.entityCount("Account", 2); // from, to
+      assert.entityCount("ReceiptOwnerBalance", 2);
 
-      assert.entityCount("Account", 3); // from, to, operator
-      assert.entityCount("ReceiptTransfer", 1);
-      assert.fieldEquals("ReceiptTransfer", id, "from", FROM.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id, "to", TO.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id, "operator", OPERATOR.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id, "amount", "100");
-      assert.fieldEquals("ReceiptTransfer", id, "tokenId", "1");
+      const fromId = createReceiptOwnerBalanceId(transferEvent.address, transferEvent.params.id, transferEvent.params.from).toHexString();
+      const toId = createReceiptOwnerBalanceId(transferEvent.address, transferEvent.params.id, transferEvent.params.to).toHexString();
+
+      assert.fieldEquals("ReceiptOwnerBalance", fromId, "receiptAddress", RECEIPT_ADDRESS.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", fromId, "owner", FROM.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", fromId, "balance", "-100");
+      assert.fieldEquals("ReceiptOwnerBalance", fromId, "tokenId", "1");
+
+      assert.fieldEquals("ReceiptOwnerBalance", toId, "receiptAddress", RECEIPT_ADDRESS.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", toId, "owner", TO.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", toId, "balance", "100");
+      assert.fieldEquals("ReceiptOwnerBalance", toId, "tokenId", "1");
     });
   });
 
   describe("batch", () => {
-    test("should build and store ReceiptTransfer item from BatchTransfer event", () => {
+    test("should build, apply balance changes and store ReceiptOwnerBalance items from BatchTransfer event", () => {
       let transferEvent = createReceiptTransferBatchEvent(
         RECEIPT_ADDRESS,
         FROM,
@@ -57,22 +59,34 @@ describe("ReceiptTransfer handling", () => {
       );
       handleReceiptTransferBatch(transferEvent);
 
-      const id0 = getId(transferEvent.transaction.hash, transferEvent.logIndex, 0).toHexString();
-      const id1 = getId(transferEvent.transaction.hash, transferEvent.logIndex, 1).toHexString();
+      assert.entityCount("Account", 2); // from, to
+      assert.entityCount("ReceiptOwnerBalance", 4);
 
-      assert.entityCount("Account", 3); // from, to, operator
-      assert.entityCount("ReceiptTransfer", 2);
-      assert.fieldEquals("ReceiptTransfer", id0, "from", FROM.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id0, "to", TO.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id0, "operator", OPERATOR.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id0, "amount", "100");
-      assert.fieldEquals("ReceiptTransfer", id0, "tokenId", "1");
+      const fromId0 = createReceiptOwnerBalanceId(transferEvent.address, transferEvent.params.ids[0], transferEvent.params.from).toHexString();
+      const toId0 = createReceiptOwnerBalanceId(transferEvent.address, transferEvent.params.ids[0], transferEvent.params.to).toHexString();
 
-      assert.fieldEquals("ReceiptTransfer", id1, "from", FROM.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id1, "to", TO.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id1, "operator", OPERATOR.toHexString());
-      assert.fieldEquals("ReceiptTransfer", id1, "amount", "200");
-      assert.fieldEquals("ReceiptTransfer", id1, "tokenId", "2");
+      assert.fieldEquals("ReceiptOwnerBalance", fromId0, "receiptAddress", RECEIPT_ADDRESS.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", fromId0, "owner", FROM.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", fromId0, "balance", "-100");
+      assert.fieldEquals("ReceiptOwnerBalance", fromId0, "tokenId", "1");
+
+      assert.fieldEquals("ReceiptOwnerBalance", toId0, "receiptAddress", RECEIPT_ADDRESS.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", toId0, "owner", TO.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", toId0, "balance", "100");
+      assert.fieldEquals("ReceiptOwnerBalance", toId0, "tokenId", "1");
+
+      const fromId1 = createReceiptOwnerBalanceId(transferEvent.address, transferEvent.params.ids[1], transferEvent.params.from).toHexString();
+      const toId1 = createReceiptOwnerBalanceId(transferEvent.address, transferEvent.params.ids[1], transferEvent.params.to).toHexString();
+
+      assert.fieldEquals("ReceiptOwnerBalance", fromId1, "receiptAddress", RECEIPT_ADDRESS.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", fromId1, "owner", FROM.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", fromId1, "balance", "-200");
+      assert.fieldEquals("ReceiptOwnerBalance", fromId1, "tokenId", "2");
+
+      assert.fieldEquals("ReceiptOwnerBalance", toId1, "receiptAddress", RECEIPT_ADDRESS.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", toId1, "owner", TO.toHexString());
+      assert.fieldEquals("ReceiptOwnerBalance", toId1, "balance", "200");
+      assert.fieldEquals("ReceiptOwnerBalance", toId1, "tokenId", "2");
     });
   });
 });
